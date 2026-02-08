41765166fe40a5e9bba5823273323bcf2666aa4c
lisovskiy <lisovskiy@mailo.com>
2026-02-08 17:29:57 +0500

    link c++ instead of stdc++

--- skia-bindings/build_support/binaries_config.rs
+++ skia-bindings/build_support/binaries_config.rs
@@ -124,7 +124,7 @@ impl BinariesConfiguration {
 
         cargo::add_static_link_libs(&target, self.built_libraries(true));
         cargo::add_static_link_libs(&target, &self.link_libraries);
-        cargo::add_link_libs(["stdc++"]);
+        cargo::add_link_libs(["c++"]);
     }
 
     /// Import library and additional files from `from_dir` to the output directory.
